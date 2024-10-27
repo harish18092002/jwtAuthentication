0f3c9793383097a9dd1102bb5e65860e2f677def
harish <harish18092002@gmail.com>
2024-10-27 21:41:44 +0530

    TypeOrm initiated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './user-entity';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'root',
-      database: 'mydb',
-      entities: [User],
-      synchronize: true,
-    }),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+const dbURl = process.env.JWT_DB_URL;
