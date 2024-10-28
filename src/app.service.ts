cde7df39054ab5cdfadc5d819ed1cbc54511829a
harish <harish18092002@gmail.com>
2024-10-28 10:21:10 +0530

    Raw sql is used for db

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import { Pool } from 'mysql2';
+const logger = new Logger('JWT-User-Service');
 
 @Injectable()
 export class AppService {
+  constructor(@Inject('DATABASE_POOL') private pool: Pool) {}
+  async createUser(username: string, password: string) {
+    try {
+      const query =
+        'INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *';
+      const values = [username, password];
+      logger.error(username, password);
+      const result = await this.pool.query(query, values);
+
+      return result;
+    } catch (error) {
+      throw new Error(`Error creating user: ${error.message}`);
+    }
+  }
   getHello(): string {
     return 'Hello World!';
   }
