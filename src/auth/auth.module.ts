2bf03bf979fd78ba7ab6d736950903a64ec4e5f1
harish <harish18092002@gmail.com>
2024-11-03 21:25:58 +0530

    Added auth modele and initialised it

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: 'abc123xyz',
+      signOptions: { expiresIn: '1hr' },
+    }),
+  ],
+
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class AuthModule {}
