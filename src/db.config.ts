cdfeeaf18d31e9946b11454d4cf71ecfbe33bfef
harish <harish18092002@gmail.com>
2024-11-12 13:29:08 +0530

    Added hased password using argon2

--- src/db.config.ts
+++ src/db.config.ts
@@ -25,7 +25,7 @@ export class DatabaseModule implements OnModuleInit {
     try {
       await this.pool.query(`
         CREATE TABLE IF NOT EXISTS jwtusers (
-          id SERIAL PRIMARY KEY,
+          id  PRIMARY KEY,
           username VARCHAR(255) NOT NULL UNIQUE,
           password VARCHAR(255) NOT NULL,
           created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
