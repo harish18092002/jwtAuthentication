776826b758a2bc96e70bd85e404b715fd62ad7fa
harish <harish18092002@gmail.com>
2024-11-24 20:45:19 +0530

    Handled jwt in perfect manner

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,18 +1,26 @@
+// auth.module.ts
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    JwtModule.register({
-      secret: 'abfdsac123xyz',
-      signOptions: {
-        expiresIn: '30d',
-      },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: process.env.JWT_SECRET || 'abfdsac123xyz',
+        signOptions: {
+          expiresIn: '30d',
+        },
+        verifyOptions: {
+          ignoreExpiration: false, // Important: Make sure we check expiration
+        },
+      }),
+      inject: [ConfigService],
     }),
   ],
-
   controllers: [AuthController],
   providers: [AuthService],
 })
