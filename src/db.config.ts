d6cc71f5d25eba300083062539d77868362e3ebf
harish <harish18092002@gmail.com>
2024-11-26 23:23:08 +0530

    Added user based access code

--- src/db.config.ts
+++ src/db.config.ts
@@ -27,6 +27,7 @@ export class DatabaseModule implements OnModuleInit {
           id VARCHAR(255) PRIMARY KEY,
           username VARCHAR(255) NOT NULL UNIQUE,
           password VARCHAR(255) NOT NULL,
+          role VARCHAR(255) NOT NULL,
           created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         );
       `);
